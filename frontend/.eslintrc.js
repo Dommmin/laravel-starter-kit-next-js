65287a3744943b5fba1f1978e25cf48169109181
Dominik <d.jasinski@ideo.pl>
2024-05-17 11:27:47 +0200

    feat: update eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,34 +1,38 @@
 eslintConfig = {
    root: true,
-   env: {
-      node: true,
-      browser: true,
-      es6: true,
-   },
-   extends: ['eslint:recommended', 'next/core-web-vitals'],
-   ignorePatterns: ['dist', '.eslintrc.js'],
-   parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-         jsx: true,
-      },
-   },
+   parser: '@babel/eslint-parser',
    settings: {
       react: {
          version: 'detect',
       },
    },
-   plugins: ['react-refresh', 'prettier'],
+   env: {
+      node: true,
+      browser: true,
+      es6: true,
+      commonjs: true,
+   },
+   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
    rules: {
       'import/prefer-default-export': 0,
       'no-console': 'warn',
       'no-nested-ternary': 0,
       'no-underscore-dangle': 0,
-      'no-unused-expressions': ['error', { allowTernary: true }],
+      'no-unused-vars': 'warn',
+      'no-unused-expressions': [
+         'error',
+         {
+            allowTernary: true,
+         },
+      ],
       camelcase: 0,
       'react/self-closing-comp': 1,
-      'react/jsx-filename-extension': [1, { extensions: ['.js', 'jsx'] }],
+      'react/jsx-filename-extension': [
+         1,
+         {
+            extensions: ['.js', 'jsx'],
+         },
+      ],
       'react/prop-types': 0,
       'react/destructuring-assignment': 0,
       'react/jsx-no-comment-textnodes': 0,
@@ -36,9 +40,28 @@ eslintConfig = {
       'react/no-array-index-key': 0,
       'react/no-unescaped-entities': 0,
       'react/require-default-props': 0,
-      'react/react-in-jsx-scope': 0,
-      'linebreak-style': ['error', 'unix'],
+      'react/react-in-jsx-scope': 'off',
+      'linebreak-style': 'off',
       semi: ['error', 'always'],
-      'prettier/prettier': ['error', { endOfLine: 'auto' }, { usePrettierrc: true }],
+      'prettier/prettier': [
+         'error',
+         {
+            endOfLine: 'auto',
+         },
+         {
+            usePrettierrc: true,
+         },
+      ],
    },
+   parserOptions: {
+      ecmaFeatures: {
+         jsx: true,
+      },
+      ecmaVersion: 2020,
+      requireConfigFile: false,
+      babelOptions: {
+         presets: ['@babel/preset-react'],
+      },
+   },
+   plugins: ['react', '@next/eslint-plugin-next', 'prettier'],
 };
