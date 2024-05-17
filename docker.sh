3b3d700ca8afdbd7aff0e10c428fd133e7ed4fa6
Dominik <d.jasinski@ideo.pl>
2024-05-17 12:39:32 +0200

    feat: update docker

--- docker.sh
+++ docker.sh
@@ -24,15 +24,17 @@ eval "$(grep ^DBNAME= $ENV_FILE)"
 eval "$(grep ^DOCKER_IP= $ENV_FILE)"
 eval "$(grep ^DOCKER_PORT= $ENV_FILE)"
 
+DOCKER_PREFIX=$(echo $DOCKER_PREFIX | tr -d '[:space:]')
+
 # Uruchomienie kontenerów dla projektu
 echo "${YELLOW}Uruchomienie kontenerów dla projektu ${BOLD}${DOCKER_PREFIX}${RESET}"
 docker-compose up -d
 
 # Dodanie użytkownika, jeśli nie istnieje
-if ! docker exec -it -u root "${DOCKER_PREFIX}_api" id -u $USER > /dev/null 2>&1; then
+if ! docker exec -it -u root "$DOCKER_PREFIX"_api id -u $USER > /dev/null 2>&1; then
     echo -e "${BOLD}${YELLOW}Dodanie użytkownika ${USER}${RESET}\n"
-    docker exec -it -u root "${DOCKER_PREFIX}_api" addgroup -g $USER_ID $USER
-    docker exec -it -u root "${DOCKER_PREFIX}_api" adduser -u $USER_ID -G $USER -h /home/$USER -D $USER
+    docker exec -it -u root "$DOCKER_PREFIX"_api addgroup -g $USER_ID $USER
+    docker exec -it -u root "$DOCKER_PREFIX"_api adduser -u $USER_ID -G $USER -h /home/$USER -D $USER
 else
     echo "${BOLD}${RED}Użytkownik ${USER} już istnieje${RESET}"
 fi
@@ -40,7 +42,7 @@ fi
 # Zmiana uprawnien
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Zmiana uprawnien${RESET}\n"
-docker exec -it -u root "${DOCKER_PREFIX}_api" chown -R $USER:$USER storage
+docker exec -it -u root "$DOCKER_PREFIX"_api chown -R $USER:$USER storage
 
 # Utworzenie katalogu dla Composera i zmiana właściciela
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
@@ -56,12 +58,7 @@ docker exec -it -u $USER "${DOCKER_PREFIX}_api" composer install --no-scripts
 # Uruchomienie migracji
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Uruchomienie migracji${RESET}\n"
-docker exec -u www-data "$DOCKER_PREFIX"_api bash migration.sh
-
-# Instalacja zależności npm
-echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
-echo -e "${BOLD}${YELLOW}Instalacja zależności npm${RESET}\n"
-docker exec -it "${DOCKER_PREFIX}_frontend" npm install && npm run build
+docker exec -u root "$DOCKER_PREFIX"_api bash migration.sh
 
 # CS Fixer
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
@@ -72,6 +69,11 @@ docker exec -it "$DOCKER_PREFIX"_api composer pint
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
 echo -e "${BOLD}${YELLOW}Insights${RESET}\n"
 docker exec -it "$DOCKER_PREFIX"_api php artisan insights --fix
+
+# Instalacja zależności npm
+echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
+echo -e "${BOLD}${YELLOW}Instalacja zależności npm${RESET}\n"
+docker exec -it "${DOCKER_PREFIX}_frontend" npm install
 
 # Lint
 echo "${BOLD}${RED}--------------------------------------------------------------------------------${RESET}"
